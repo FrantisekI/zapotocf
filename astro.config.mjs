589014bdd0cb85740e517976ba11e8e1d4cb1bd9
FrantisekI <148909995+FrantisekI@users.noreply.github.com>
2024-12-28 15:51:42 +0100

    eddit to mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+// // https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://example.com',
+// 	integrations: [mdx(), sitemap()],
+// });
+
+import { defineConfig } from 'astro/config';
+
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  output: 'static' // This is actually the default
 });
